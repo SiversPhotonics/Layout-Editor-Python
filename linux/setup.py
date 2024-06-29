8ac56679a00b3bedb4c14d3a36b1a90fadbd5313
Matthew McKee <matthewmckee04@yahoo.co.uk>
2024-06-29 22:42:23 +0100

    feat: add type hints for boolean tool, point, pointArray

--- linux/setup.py
+++ linux/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="layout_editor",
-    version="1.0",
+    version="1.0.1",
     include_package_data=True,
     packages=find_packages(),
     description="A layout editor package",
