c055a392cd123286a063343480e66f87efaea26e
Matthew McKee <matthewmckee04@yahoo.co.uk>
2024-07-01 20:24:34 +0100

    only use one dir now, instead of linux and windows

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import find_packages, setup
+
+package_data = {}
+
+if os.name == "posix":
+    package_data = {"": ["*.so", "py.typed", "platforms/*"]}
+elif os.name == "nt":
+    package_data = {"": ["*.pyd", "py.typed", "platforms/*"]}
+
+setup(
+    name="layout_editor",
+    version="1.0.1",
+    include_package_data=True,
+    packages=find_packages(),
+    description="A layout editor package",
+    author="Matthew McKee",
+    author_email="matthew.mckee@sivers-photonics.com",
+    install_requires=["numpy"],
+    package_data={"": ["*.pyd", "py.typed", "platforms/*"]},
+)
